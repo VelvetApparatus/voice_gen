a7659a475d44ffba2565430d6cef152c63422f07
root <you@example.com>
2026-01-04 12:55:58 +0000

    update scripts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -15,7 +15,7 @@ python3 -m scripts.generate_malay -in ${MALAY_CSV_PATH} -out ${MALAY_OUT_DIR} -n
 WELSH_CSV_PATH="data/welsh/welsh_12000.csv"
 WELSH_OUT_DIR="gen/welsh"
 
-python3 -m scripts.generate_welsh -in ${WELSH_CSV_PATH} -out ${WELSH_OUT_DIR} -n welsh
+# python3 -m scripts.generate_welsh -in ${WELSH_CSV_PATH} -out ${WELSH_OUT_DIR} -n welsh
 
 
 # FRENCH RUN
@@ -23,7 +23,7 @@ python3 -m scripts.generate_welsh -in ${WELSH_CSV_PATH} -out ${WELSH_OUT_DIR} -n
 FRENCH_CSV_PATH="data/french/french_4500.csv"
 FRENCH_OUT_DIR="gen/french"
 
-python3 -m scripts.generate_french -in ${FRENCH_CSV_PATH} -out ${FRENCH_OUT_DIR} -n french
+# python3 -m scripts.generate_french -in ${FRENCH_CSV_PATH} -out ${FRENCH_OUT_DIR} -n french
 
 
 # TELUGU RUN
@@ -31,7 +31,7 @@ python3 -m scripts.generate_french -in ${FRENCH_CSV_PATH} -out ${FRENCH_OUT_DIR}
 TELUGU_CSV_PATH="data/telugu/telugu_12000.csv"
 TELUGU_OUT_DIR="gen/telugu"
 
-# python3 -m scripts.generate_telugu -in ${TELUGU_CSV_PATH} -out ${TELUGU_OUT_DIR} -l 1 -n telugu
+python3 -m scripts.generate_telugu -in ${TELUGU_CSV_PATH} -out ${TELUGU_OUT_DIR}  -n telugu
 
 
 # AVAR RUN
@@ -39,4 +39,4 @@ TELUGU_OUT_DIR="gen/telugu"
 AVAR_CSV_PATH="data/avar/avar_12000.csv"
 AVAR_OUT_DIR="gen/avar"
 
-python3 -m scripts.generate_avar -in ${AVAR_CSV_PATH} -out ${AVAR_OUT_DIR} -n avar
+# python3 -m scripts.generate_avar -in ${AVAR_CSV_PATH} -out ${AVAR_OUT_DIR} -n avar
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-python -m venv venv
+python3.11 -m venv venv
 source venv/bin/activate
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
 
 pip install git+https://github.com/huggingface/parler-tts.git
 
-pip install -r requirements.txt
